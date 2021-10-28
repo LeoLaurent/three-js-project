d3ffd00bcde33a89528e7fa6d35bbaf61b2e866a
Kapii1 <89147630+Kapii1@users.noreply.github.com>
2021-10-28 08:05:58 +0200

    Commentaire : Anthony

--- app-vite/main.js
+++ app-vite/main.js
@@ -1,5 +1,8 @@
 import * as THREE from 'three';
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
+
+// Définition des variables globales
+
 let camera, scene, renderer, controls;
 
 const objects = [];
@@ -18,27 +21,36 @@ const direction = new THREE.Vector3();
 const vertex = new THREE.Vector3();
 const color = new THREE.Color();
 
-
+// Appel des fonctions
 init();
 animate();
 
+// Initialisation
 function init() {
+    // Création scène
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0x87CEFA)
-    camera = new THREE.PerspectiveCamera(110, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.y = 10
+
     const color = 0x87CEFA;  // white
     const near = 1;
     const far = 200;
     const density = 0.1;
     scene.fog = new THREE.Fog(color, near, far);
   
+    
+    // Création Caméra
+    camera = new THREE.PerspectiveCamera(110, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.y = 10
+
+
+    // Création renderer
     renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true
     document.body.appendChild(renderer.domElement)
     renderer.setPixelRatio(devicePixelRatio)
 
+    // Création texture sol
     const textureloaderWood = new THREE.TextureLoader()
     const textureBasecolorWood = textureloaderWood.load('img/Wood_Floor_009_basecolor.jpg')
     const textureAmbientOcclussionWood = textureloaderWood.load('img/Wood_Floor_009_ambientOcclusion.jpg')
@@ -46,6 +58,7 @@ function init() {
     const texturenormalWood = textureloaderWood.load('img/Wood_Floor_009_normal.jpg')
     const textureroughnessWood = textureloaderWood.load('img/Wood_Floor_009_roughness.jpg')
 
+    // Répétition texture sol pour recouvrement
     textureBasecolorWood.wrapS = textureBasecolorWood.wrapT=THREE.RepeatWrapping
     textureBasecolorWood.repeat.set(15,15)
     textureAmbientOcclussionWood.wrapS = textureAmbientOcclussionWood.wrapT=THREE.RepeatWrapping
@@ -57,6 +70,7 @@ function init() {
     textureroughnessWood.wrapS = textureroughnessWood.wrapT=THREE.RepeatWrapping
     textureroughnessWood.repeat.set(15,15 )
 
+    // Création sol
     const plane_geometry = new THREE.PlaneGeometry(1000, 1000);
     const plane_material = new THREE.MeshStandardMaterial(
         {
@@ -73,7 +87,7 @@ function init() {
     plane.rotateX( - Math.PI / 2 );
     scene.add(plane);
 
-    
+    // Création texture cube
     const textureloader = new THREE.TextureLoader()
     const textureBasecolor = textureloader.load('img/Concrete_Blocks_011_basecolor.jpg')
     const texturenormal = textureloader.load('img/Concrete_Blocks_011_ambientOcclusion.jpg')
@@ -81,7 +95,7 @@ function init() {
     const textureroughness = textureloader.load('img/Concrete_Blocks_011_normal.jpg')
     const textureAmbientOcclussion = textureloader.load('img/Concrete_Blocks_011_roughness.jpg')
 
-
+    // Création Geometry et material du cube
     const cube_geometry = new THREE.BoxGeometry(10, 10, 10, 512/2, 512/2)
     const cube_material = new THREE.MeshStandardMaterial(
         {
@@ -93,7 +107,9 @@ function init() {
             aoMap: textureAmbientOcclussion
         }
     );
+    
 
+    // Création des cubes
     for ( let i = 0; i < 100; i ++ ) {
 
         const cube = new THREE.Mesh( cube_geometry, cube_material );
@@ -108,9 +124,10 @@ function init() {
 
     };
 
-
+    // Ombre douce
     renderer.shadowMapSoft = true;
     
+    // Création des controles
     controls = new PointerLockControls( camera ,  document.body );
 
     document.body.addEventListener( 'click', function () {
@@ -119,6 +136,7 @@ function init() {
     }, false );
     scene.add( controls.getObject() );
 
+    // Création des lumiètes
     const light = new THREE.DirectionalLight(0xffffff, 1, 100);
     light.position.set(20, 400,200);
     light.castShadow = true;
@@ -192,10 +210,12 @@ function init() {
     document.addEventListener( 'keydown', onKeyDown );
     document.addEventListener( 'keyup', onKeyUp );
 
+    // Initialisation des "pieds"
     raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
     window.addEventListener( 'resize', onWindowResize );    
 };
-    
+
+// Responsiveness
 function onWindowResize() {
 
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -204,18 +224,19 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-
+// Animation (personnage)
     function animate() {
 
         requestAnimationFrame( animate );
-    
+        // Création de time pour avoir le temps entre chaque rendu et recalculer
         const time = performance.now();
-    
+        // Verification si control locked
         if ( controls.isLocked === true ) {
     
             raycaster.ray.origin.copy( controls.getObject().position );
             raycaster.ray.origin.y -= 10;
-    
+            
+            // Constante pour connaitre intersection des pieds
             const intersections = raycaster.intersectObjects( objects, false );
     
             const onObject = intersections.length > 0;
@@ -225,25 +246,32 @@ function onWindowResize() {
             velocity.x -= velocity.x * 10.0 * delta;
             velocity.z -= velocity.z * 10.0 * delta;
     
-            velocity.y -= 6* 100.0 * delta; // 100.0 = mass
-    
+            velocity.y -= 6* 100.0 * delta; // 100.0 = masse
+            
+            // Transfo du bool en 1 0 -1
             direction.z = Number( moveForward ) - Number( moveBackward );
             direction.x = Number( moveRight ) - Number( moveLeft );
-            direction.normalize(); // this ensures consistent movements in all directions
-    
+            console.log(direction.x)
+            direction.normalize();
+
+            
+            // Choix de la vitesse
             if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
             if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
-    
+            
+            // Si sur objet, on peut sauter
             if ( onObject === true ) {
     
                 velocity.y = Math.max( 0, velocity.y );
                 canJump = true;
     
             }
-    
+            
+            // Application de la vitesse pour bouger
             controls.moveRight( - velocity.x * delta );
             controls.moveForward( - velocity.z * delta );
-    
+            
+            // Saut
             controls.getObject().position.y += ( velocity.y * delta ); // new behavior
     
             if ( controls.getObject().position.y < 10 ) {
